6c4802ac64873ecd5e2dd3c334209d15c11932a9
Shawn Almeida <shawn9920@gmail.com>
2023-09-21 15:18:17 +0530

    just some cleanup

--- server/api/middleware/requireAuth.js
+++ server/api/middleware/requireAuth.js
@@ -1,27 +1,6 @@
 const jwt = require("jsonwebtoken");
 const users = require("../models/users");
 
-// const requireAuth = async (req, res, next) => {
-//   try {
-//     //Read the token off the cookie
-
-//     console.log("req.cookies==>", req.cookies);
-//     const token = req.cookies.Authorization;
-//     //decode the token
-//     const decoded = jwt.verify(token, process.env.SECRET);
-//     console.log("req.cookie==>", decoded);
-//     //Find the user decoded sub
-//     const user = await users.findById(decoded.encode);
-//     if (!user) return res.sendStatus(401);
-
-//     //connect the user to the req
-//     req.user = user;
-//     //continue on
-//     next();
-//   } catch (error) {
-//     return res.status(401).send({ message: error });
-//   }
-// };
 const requireAuth = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
