ad692961b2cec4a74f3bc1d97874b6491c376e8c
Shawn Almeida <shawn9920@gmail.com>
2023-10-09 20:38:57 +0530

    switched URLs

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,9 +14,9 @@ export default defineConfig({
   },
   define: {
     "process.env": {
-      REACT_APP_UAT_URL: "http://localhost:8080",
-      // REACT_APP_UAT_URL:
-      //   "https://inklothes-app-backend-y2xvt.ondigitalocean.app",
+      // REACT_APP_UAT_URL: "http://localhost:8080",
+      REACT_APP_UAT_URL:
+        "https://inklothes-app-backend-y2xvt.ondigitalocean.app",
       REACT_APP_ENCRYPTION: "WABBALABBA@3344$$1DUB43DUB",
     },
   },
