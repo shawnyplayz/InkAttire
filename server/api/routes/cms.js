5fcb7de92e108d811e52f2fd849d676de2f0fb97
Shawn Almeida <shawn9920@gmail.com>
2023-11-08 04:20:38 +0530

    fixed featured products and carousel, still to be tested!

--- server/api/routes/cms.js
+++ server/api/routes/cms.js
@@ -6,20 +6,22 @@ const cloudinary = require("../../utils/cloudinary");
 
 const getCarousel = async (req, res) => {
   var distinctValues = await products.distinct("genre");
-  let getFeatured = await products.find({"genre":{$in:distinctValues}})
+  let getFeatured = await products.find({ genre: { $in: distinctValues } });
   const result = await cloudinary.api.resources({
-    type: 'upload',
+    type: "upload",
     prefix: "carousel",
   });
-
-  // const pushFeatured = new cms({
-  //   featuredProducts : [...getFeatured]
-  // })
-  try {    
-    await cms.updateOne({
-      $set:{featuredProducts:[...getFeatured]},
-      $set:{carousel:[...result?.resources]}
-    });
+  await cms.deleteMany({});
+  const pushFeatured = new cms({
+    carousel: [...result?.resources],
+    featuredProducts: [...getFeatured],
+  });
+  try {
+    await pushFeatured.save();
+    // await cms.updateOne({
+    //   $set: { featuredProducts: [...getFeatured] },
+    //   $set: { carousel: [...result?.resources] },
+    // });
     let getCar = await cms.find({});
     res.status(200).json(...getCar);
   } catch (error) {
@@ -29,39 +31,50 @@ const getCarousel = async (req, res) => {
 
 const postCarousel = async (req, res) => {
   try {
- const uploadPromises = req.body.carousel?.map((base64Data) => {
+    const uploadPromises = req.body.carousel?.map((base64Data) => {
       // Upload each image to Cloudinary
       return cloudinary.uploader.upload(base64Data, {
-        folder: 'carousel', // Specify the folder for uploaded images
+        folder: "carousel", // Specify the folder for uploaded images
       });
     });
 
     const uploadedImages = await Promise.all(uploadPromises);
     await cms
-        .updateOne({ $push: { carousel: { $each: [{
-          url: uploadedImages.secure_url,
-          public_id: uploadedImages.public_id,
-      }]} } })
-        .then(() => {
-          return res.status(201).json({
-            message: "Updated Successfully!",
-          });
+      .updateOne({
+        $push: {
+          carousel: {
+            $each: [
+              {
+                url: uploadedImages.secure_url,
+                public_id: uploadedImages.public_id,
+              },
+            ],
+          },
+        },
+      })
+      .then(() => {
+        return res.status(201).json({
+          message: "Updated Successfully!",
         });
+      });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
-const deleteCarousel  =  async (req, res, next) => {
+const deleteCarousel = async (req, res, next) => {
   try {
-    console.log("req.params.id",req.body.id);
-const result = await cloudinary.uploader.destroy(req.body.id, function(result) { console.log(result) });
-if(result.result == "ok"){
-  res.status(200).send({ message: result});
-}
-else{
-  res.status(401).send({ message: result.result});
-
-}
+    console.log("req.params.id", req.body.id);
+    const result = await cloudinary.uploader.destroy(
+      req.body.id,
+      function (result) {
+        console.log(result);
+      }
+    );
+    if (result.result == "ok") {
+      res.status(200).send({ message: result });
+    } else {
+      res.status(401).send({ message: result.result });
+    }
     // const getOne = await cms.find({ carousel: {
     //   $elemMatch: {
     //     img_id: req.params.id
@@ -83,10 +96,9 @@ else{
     res.status(500).json({ message: error.message });
   }
   next();
-
 };
 module.exports = {
   getCarousel,
   postCarousel,
-  deleteCarousel
+  deleteCarousel,
 };
