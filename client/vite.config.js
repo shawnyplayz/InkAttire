7487b37dca85dc12c2aa61d609230d0b3817048b
Shawn Almeida <shawn9920@gmail.com>
2023-12-17 13:35:41 +0530

    reworked hosting links

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   define: {
     "process.env": {
       // REACT_APP_UAT_URL: "http://localhost:8080",
-      REACT_APP_UAT_URL: "https://api.inklothes.com/",
+      REACT_APP_UAT_URL: "https://api.inklothes.com",
       REACT_APP_ENCRYPTION: "WABBALABBA@3344$$1DUB43DUB",
     },
   },
